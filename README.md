00aac00aec8ea8445f70b999ccaab91d295589d6
ex1234 <4191524+ex1234@users.noreply.github.com>
2025-10-01 10:40:32 +0200

    init: project scaffold

---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# File Renamer
+Batch rename files by adding prefix/suffix.
+## Usage
+python rename.py folder prefix_ _suffix
---
+++ rename.py
@@ -0,0 +1,6 @@
+﻿import sys, pathlib
+if len(sys.argv)<4: print('Usage: rename.py <dir> <prefix> <suffix>'); raise SystemExit
+d, pre, suf = map(pathlib.Path, [sys.argv[1]]), sys.argv[2], sys.argv[3]
+for p in d[0].iterdir():
+    if p.is_file():
+        p.rename(p.with_name(pre + p.stem + suf + p.suffix))
